0436bc29119aa839da4313fe2a4f7ad151d8c856
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-03-06 17:01:21 +0100

    build script fixes

--- ci/build.sh
+++ ci/build.sh
@@ -1,20 +1,20 @@
 
 mkdir -p dist
 
-suffix=
+suffix=""
 
-if [ ! -z TRAVIS_COMMIT ]; then
+if [ ! -z "$TRAVIS_COMMIT" ]; then
    suffix=_$TRAVIS_COMMIT
-endif
-if [ ! -z $TRAVIS_TAG ]; then
+fi
+if [ ! -z "$TRAVIS_TAG" ]; then
    suffix=_$TRAVIS_TAG
 fi
 
 # copy the example to the root so we can build with CLI, replacing include "swd.h" with include "swd/swd.h"
-cat firmware/examples/Norwegian_Blue.cpp | sed 's#include "swd/swd.h"#include "swd.h"#g' > firmware/Norwegian_Blue.cpp
+# cat firmware/examples/Norwegian_Blue.cpp | sed 's#include "swd/swd.h"#include "swd.h"#g' > firmware/Norwegian_Blue.cpp
 
 for p in photon p1 electron
 do
-   particle compile $p firmware --saveTo dist/Norwegian_Blue_$p$suffix.bin
+  particle compile $p firmware --saveTo dist/Norwegian_Blue_$p$suffix.bin
 done
 
