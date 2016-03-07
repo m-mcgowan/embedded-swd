dd502f9258d1eaa675792e4572d5fb0ddf7d0a8c
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-03-07 22:02:53 +0100

    reintroduces commented out code so the full example app is built

--- ci/build.sh
+++ ci/build.sh
@@ -11,7 +11,7 @@ if [ ! -z "$TRAVIS_TAG" ]; then
 fi
 
 # copy the example to the root so we can build with CLI, replacing include "swd.h" with include "swd/swd.h"
-# cat firmware/examples/Norwegian_Blue.cpp | sed 's#include "swd/swd.h"#include "swd.h"#g' > firmware/Norwegian_Blue.cpp
+cat firmware/examples/Norwegian_Blue.cpp | sed 's#include "swd/swd.h"#include "swd.h"#g' > firmware/Norwegian_Blue.cpp
 
 for p in photon p1 electron
 do
